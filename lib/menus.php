45b949444a0cfe884d5b8fb3d68c40abab120b1d
Harry Metcalfe <harry@thedextrousweb.com>
2013-06-21 13:42:03 +0100

    Commenting out un-used menus. Solves #1554

--- lib/menus.php
+++ lib/menus.php
@@ -4,10 +4,10 @@
 add_action('init', function () {
   unregister_nav_menu('primary_navigation');
 
-  register_nav_menus(array(
-    'main_menu' => 'Main menu',
-    'related_blogs' => 'Related blogs',
-    'follow_us' => 'Follow us',
-    'footer' => 'Footer',
-  ));
+  //register_nav_menus(array(
+  // 'main_menu' => 'Main menu',
+  //  'related_blogs' => 'Related blogs',
+  //  'follow_us' => 'Follow us',
+  //  'footer' => 'Footer',
+  //));
 });
