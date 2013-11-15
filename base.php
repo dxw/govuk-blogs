505d4fee81e9838ac37275a37c40c3b7ebf30564
ryan.holder <ryan@dxw.com>
2013-11-15 13:34:30 +0000

    move shortcut icon link to before wp_head()

--- base.php
+++ base.php
@@ -7,8 +7,9 @@
     <meta charset="utf-8">
     <title><?php wp_title('|', true, 'right') ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <?php wp_head() ?>
     <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico" />
+    <?php wp_head() ?>
+    
   </head>
   <body <?php body_class() ?>>
 
