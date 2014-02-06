226fe415ebe3bda106fd24d603ee4c41e256dfe1
Tom Adams <tom@dxw.com>
2014-02-06 16:49:43 -0500

    Hide everything except the article

--- templates/govuk.php
+++ templates/govuk.php
@@ -1,11 +1,13 @@
-<header class="govuk full-width">
-  <div class="container">
-    <div class="row">
+<div class="govuk-header">
+  <header class="govuk full-width">
+    <div class="container">
+      <div class="row">
 
-      <p class="span12"><a class="ir" href="https://www.gov.uk/">GOV.UK</a></p>
+        <p class="span12"><a class="ir" href="https://www.gov.uk/">GOV.UK</a></p>
 
+      </div>
     </div>
-  </div>
-</header>
-<div class="js-cookies-banner"></div>
-<div class="container bar"></div>
+  </header>
+  <div class="js-cookies-banner"></div>
+  <div class="container bar"></div>
+</div>
