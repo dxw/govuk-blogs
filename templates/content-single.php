5f484532014025e8009c983509522cdab3091854
Tom Adams <tom@dxw.com>
2014-02-17 11:30:41 -0500

    Add a tiny bit of CSS

--- templates/content-single.php
+++ templates/content-single.php
@@ -11,12 +11,15 @@
       <?php the_content() ?>
     </div>
     <footer class="single">
-      <ul class="related-posts">
-        <?php while (have_rows('related_posts')) : ?>
-          <?php the_row() ?>
-          <li><a href="<?php echo esc_attr(get_sub_field('url')) ?>"><?php echo esc_html(get_sub_field('title')) ?></a></li>
-        <?php endwhile ?>
-      </ul>
+      <div class="related-posts">
+        <p>You may also be interested in:</p>
+        <ul>
+          <?php while (have_rows('related_posts')) : ?>
+            <?php the_row() ?>
+            <li><a href="<?php echo esc_attr(get_sub_field('url')) ?>"><?php echo esc_html(get_sub_field('title')) ?></a></li>
+          <?php endwhile ?>
+        </ul>
+      </div>
       <p>
         <?php echo get_the_tag_list('<p><strong>Tags:</strong> ', ',', '</p>'); ?>
       </p>
