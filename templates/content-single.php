2c39c13f10a24b9a66c26ee7a168935bbbdaba9d
Magda Faizov <magda@dxw.com>
2014-02-18 12:47:03 +0000

    related links and prev/next button styles

--- templates/content-single.php
+++ templates/content-single.php
@@ -1,5 +1,8 @@
 <?php while (have_posts()) : the_post() ?>
   <article <?php post_class() ?>>
+    <div class="visible-print">
+      <p><?php echo esc_html(get_permalink()) ?></p>
+    </div>
     <header>
       <h1 class="entry-title"><?php the_title() ?></h1>
       <?php get_template_part('templates/entry-meta') ?>
@@ -8,15 +11,33 @@
       <?php the_content() ?>
     </div>
     <footer class="single">
-      <p>
-        <strong>By:</strong> <?php gds_byline(); ?>
-        &nbsp;&nbsp;<strong>Category:</strong> <?php echo get_the_category_list(', ') ?>
-      </p>
+      <?php if (have_rows('related_posts')) : ?>
+        <div class="related-posts">
+          <p>You may also be interested in:</p>
+          <ul>
+            <?php while (have_rows('related_posts')) : ?>
+              <?php the_row() ?>
+              <li><a href="<?php echo esc_attr(get_sub_field('url')) ?>"><?php echo esc_html(get_sub_field('title')) ?></a></li>
+            <?php endwhile ?>
+          </ul>
+        </div>
+      <?php endif ?>
       <p>
         <?php echo get_the_tag_list('<p><strong>Tags:</strong> ', ',', '</p>'); ?>
       </p>
     </footer>
-    <?php share_icons(get_the_ID()) ?>
-    <?php comments_template('/templates/comments.php') ?>
   </article>
+
+  <div class="page-numbers-container">
+    <div class="previous pull-left">
+      <?php previous_post_link('<span class="arrow-button"></span><span class="link">%link</span>') ?>
+    </div>
+    <div class="next pull-right">
+      <?php next_post_link('<span class="link">%link</span><span class="arrow-button"></span>') ?>
+    </div>
+  </div>
+  <div class="clearfix"></div>
+
+  <?php share_icons(get_the_ID()) ?>
+  <?php comments_template('/templates/comments.php') ?>
 <?php endwhile ?>
