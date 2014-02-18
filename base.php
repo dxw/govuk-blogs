75405b984940b81b58a1961598783a5baa362d60
Tom Adams <tom@dxw.com>
2014-02-18 15:18:44 -0500

    Move base template out of base.php

--- base.php
+++ base.php
@@ -13,26 +13,7 @@ echo $template->render([
   'assetPath' => get_template_directory_uri().'/build/govuk_template/assets/',
   'head' => \Missing\String::get_output('wp_head'),
   'bodyClasses' => \Missing\String::get_output('body_class'),
-  'content' => \Missing\String::get_output(function () {
-    ?>
-    <section class="container">
-      <?php do_action('get_header') ?>
-      <?php get_template_part('templates/header') ?>
-      <?php if (is_home()) : ?>
-        <?php get_template_part('templates/featured') ?>
-      <?php endif ?>
-      <div class="row">
-        <div class="span8 main-content">
-          <?php include roots_template_path() ?>
-          <?php if(!is_single() || !is_page()) { get_template_part('templates/paging'); } ?>
-        </div>
-        <div class="span4 sidebar-contain">
-          <?php get_template_part('templates/sidebar') ?>
-        </div>
-      </div>
-    </section>
-    <?php
-  }),
+  'content' => \Missing\String::get_output(function () { get_template_part('templates/base'); }),
   'footerSupportLinks' => \Missing\String::get_output(function () { get_template_part('templates/footer'); }),
   'bodyEnd' => \Missing\String::get_output('wp_footer'),
 ]);
---
+++ templates/base.php
@@ -0,0 +1,16 @@
+<section class="container">
+  <?php do_action('get_header') ?>
+  <?php get_template_part('templates/header') ?>
+  <?php if (is_home()) : ?>
+    <?php get_template_part('templates/featured') ?>
+  <?php endif ?>
+  <div class="row">
+    <div class="span8 main-content">
+      <?php include roots_template_path() ?>
+      <?php if(!is_single() || !is_page()) { get_template_part('templates/paging'); } ?>
+    </div>
+    <div class="span4 sidebar-contain">
+      <?php get_template_part('templates/sidebar') ?>
+    </div>
+  </div>
+</section>
