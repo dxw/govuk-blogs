d93e1737495b0adff720cb67441c5c02976d767f
Tom Adams <tom@dxw.com>
2014-02-18 14:54:24 -0500

    Add the header back in

--- base.php
+++ base.php
@@ -16,6 +16,11 @@ echo $template->render([
   'content' => \Missing\String::get_output(function () {
     ?>
     <section class="container">
+      <?php do_action('get_header') ?>
+      <?php get_template_part('templates/header') ?>
+      <?php if (is_home()) : ?>
+        <?php get_template_part('templates/featured') ?>
+      <?php endif ?>
       <div class="row">
         <div class="span8 main-content">
           <?php include roots_template_path() ?>
