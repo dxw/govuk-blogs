1a78fdd12adee07ecb7436af9f6bebf9b988c42d
Tom Adams <tom@dxw.com>
2014-03-19 12:58:43 -0400

    Allow featured videos in featured posts

--- lib/thumbnail.php
+++ lib/thumbnail.php
@@ -1,9 +1,15 @@
 <?php
 
-function gds_post_thumbnail() {
+function gds_post_thumbnail($featured=false) {
   if (has_post_thumbnail()) {
-    the_post_thumbnail('large');
+    if ($featured) {
+      ?>
+      <a href="<?php echo esc_attr(get_permalink()) ?>"><?php the_post_thumbnail('large') ?></a>
+      <?php
+    } else {
+      the_post_thumbnail('large');
+    }
   } elseif ($video_url = get_post_meta(get_the_ID(), 'video_url', true)) {
-    echo $GLOBALS['wp_embed']->autoembed($video_url);
+    echo $GLOBALS['wp_embed']->shortcode([], $video_url);
   }
 }
