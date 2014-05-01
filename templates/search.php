e17410edab41fe86f3eb68bfa6a5603f9ca75747
Magda Faizov <magda@dxw.com>
2014-05-01 15:58:30 +0100

    no search results

--- templates/search.php
+++ templates/search.php
@@ -1,21 +1,31 @@
 <h2 class="search-title">Search results for <?php the_search_query(); ?> </h2>
-<?php while (have_posts()) : the_post() ?>
-<article <?php post_class() ?>>
-  <header>
-    <h2 class="entry-title"><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h2>
-    <?php get_template_part('templates/entry-meta') ?>
-    <?php if ( has_post_thumbnail() ) {
-    	the_post_thumbnail('large');
-    	} ?> 
-  </header>
-  <div class="entry-summary">
-    <?php the_excerpt() ?>
-  </div>
-  <footer>
-    <a class="read-more" href="<?php the_permalink() ?>">Read more</a>
-    —
-    <a href="<?php comments_link() ?>"><?php printf(_n('1 comment', '%1$s comments', get_comments_number(), 'roots'), number_format_i18n(get_comments_number())) ?></a>
-  </footer>
-</article>
-<?php endwhile ?>
+
+<?php if ( have_posts() ) { ?>
+  
+  <?php while (have_posts()) : the_post() ?>
+    <article <?php post_class() ?>>
+      <header>
+        <h2 class="entry-title"><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h2>
+        <?php get_template_part('templates/entry-meta') ?>
+        <?php if ( has_post_thumbnail() ) {
+        	the_post_thumbnail('large');
+        	} ?> 
+      </header>
+      <div class="entry-summary">
+        <?php the_excerpt() ?>
+      </div>
+      <footer>
+        <a class="read-more" href="<?php the_permalink() ?>">Read more</a>
+        —
+        <a href="<?php comments_link() ?>"><?php printf(_n('1 comment', '%1$s comments', get_comments_number(), 'roots'), number_format_i18n(get_comments_number())) ?></a>
+      </footer>
+    </article>
+  <?php endwhile ?>
+
+<?php } else { ?>
+  <article>
+    <p>No results found on <strong><?php echo get_bloginfo('name'); ?>.</strong></p>
+    <p>Please try searching again using different words or try this search on <a href="https://www.gov.uk/">Gov.uk</a>.</p>
+  </article>
+<?php } ?>
  
