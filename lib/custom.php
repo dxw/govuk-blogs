4a62797bf04cc18deb0680554493c8944ee7465d
Magda Faizov <magda@dxw.com>
2014-05-02 12:26:14 +0100

    reverted

--- lib/custom.php
+++ lib/custom.php
@@ -7,8 +7,7 @@ add_action('widgets_init', function () {
 
 
 # Force inserted media to use https urls
-// I have commented out for now to make the images work on the staging site. Please do not push to production!
-// add_filter('wp_get_attachment_url', function($url, $post_id) { 
-//   return preg_replace('/^http:\/\//', 'https://', $url);
-// }, 10, 2);
+add_filter('wp_get_attachment_url', function($url, $post_id) { 
+  return preg_replace('/^http:\/\//', 'https://', $url);
+}, 10, 2);
 
