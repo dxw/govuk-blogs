b3600e8d0053d8ab234fd499b4641da69e5d585a
Magda Faizov <magda@dxw.com>
2014-05-06 15:55:11 +0100

    contributors permissions

--- lib/custom.php
+++ lib/custom.php
@@ -11,3 +11,12 @@ add_filter('wp_get_attachment_url', function($url, $post_id) {
   return preg_replace('/^http:\/\//', 'https://', $url);
 }, 10, 2);
 
+
+# Contributors can upload images
+if  ( current_user_can('contributor') && !current_user_can('upload_files') )
+  add_action('admin_init', 'allow_contributor_uploads');
+
+function  allow_contributor_uploads() {
+	$contributor = get_role('contributor');
+	$contributor->add_cap('upload_files');
+}
