96f0bf85453b8bb9220c3d239ae8650d04e2ee21
Magda Faizov <magda@dxw.com>
2014-05-12 14:21:04 +0100

    categories widget on mobile and screen resize

--- assets/js/main.js
+++ assets/js/main.js
@@ -45,20 +45,47 @@ jQuery(function ($) {
     if (mobileMaxWidth < 768) {
         $('.widget_categories ul').each(function() {
             var list = $(this),
-            select = $(document.createElement('select')).insertBefore($(this).hide()).change(function() {
+            select = $(document.createElement('select')).insertBefore($(this).hide()).addClass('category-list').change(function() {
                 window.location.href = $(this).val();
             });
+            $('<option selected="selected">Select Category</option>').appendTo(select);
             $('>li a', this).each(function() {
                 var option = $(document.createElement('option'))
                 .appendTo(select)
                 .val(this.href)
                 .html($(this).html());
-                if($(this).attr('class') === 'selected') {
-                    option.attr('selected','selected');
-                }
             });
             list.remove();
         });
     }
+
+
+    // Transform category list into select and back on screen resize
+
+    $(window).resize(function() {
+        if ($(window).width() < 768){
+           $('.widget_categories ul').not('hidden-mobile').each(function() {
+                var list = $(this),
+                select = $(document.createElement('select')).insertBefore($(this).hide()).addClass('category-list').change(function() {
+                   window.location.href = $(this).val();
+                });
+                $('<option selected="selected">Select Category</option>').appendTo(select);
+                $('>li a', this).each(function() {
+                   var option = $(document.createElement('option'))
+                   .appendTo(select)
+                   .val(this.href)
+                   .html($(this).html());
+                });
+                list.remove();
+           });
+        } else {
+            $('select.category-list').parent().append('<ul class="categories"></ul>');
+            $('select.category-list option:first-child').remove();
+            $('select.category-list option').each(function(){
+              $('ul.categories').append('<li class="cat-item"><a href="' + $(this).val() + '">'+$(this).text()+'</a></li>');
+            });
+            $('select.category-list').remove();
+        }
+    });
 
 })
