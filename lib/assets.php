5906976adb53d9d107b7f54aa30607d58ff50ca8
Tom Adams <tom@dxw.com>
2014-06-18 16:43:56 -0400

    Use get_template_directory_uri() for assets

--- lib/assets.php
+++ lib/assets.php
@@ -6,11 +6,11 @@ add_action('init', function () {
 
 add_action('wp_enqueue_scripts', function () {
   wp_enqueue_script('main', get_template_directory_uri().'/build/main.min.js');
-  wp_enqueue_style('main', get_stylesheet_directory_uri().'/build/main.min.css');
+  wp_enqueue_style('main', get_template_directory_uri().'/build/main.min.css');
 });
 
 add_action('admin_enqueue_scripts', function () {
-  wp_enqueue_style('admin', get_stylesheet_directory_uri().'/build/admin.min.css');
+  wp_enqueue_style('admin', get_template_directory_uri().'/build/admin.min.css');
 });
 
 # Emit ie6 stylesheet.
