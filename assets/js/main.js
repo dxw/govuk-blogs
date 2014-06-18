869a9841e451c93df6d91a01243f9cffb3c75668
ryan.holder <ryan@dxw.com>
2014-06-18 11:40:59 +0100

    add js fix for audioboo embed styles. Delivers ticket 3086

--- assets/js/main.js
+++ assets/js/main.js
@@ -59,5 +59,66 @@ jQuery(function ($) {
 
     // Polyfills
 
-    $('input, textarea').placeholder()
+    $(window).resize(function() {
+        if ($(window).width() < 768){
+           $('.widget_categories ul').not('hidden-mobile').each(function() {
+                var list = $(this),
+                select = $(document.createElement('select')).insertBefore($(this).hide()).addClass('category-list').change(function() {
+                   window.location.href = $(this).val();
+                });
+                $('<option selected="selected">Select Category</option>').appendTo(select);
+                $('>li a', this).each(function() {
+                   var option = $(document.createElement('option'))
+                   .appendTo(select)
+                   .val(this.href)
+                   .html($(this).html());
+                });
+                list.remove();
+           });
+        } else {
+            $('select.category-list').parent().append('<ul class="categories"></ul>');
+            $('select.category-list option:first-child').remove();
+            $('select.category-list option').each(function(){
+              $('ul.categories').append('<li class="cat-item"><a href="' + $(this).val() + '">'+$(this).text()+'</a></li>');
+            });
+            $('select.category-list').remove();
+        }
+    });
+
+
+    // Roots theme meta data height
+
+    var highestMeta = 0;
+    $('.featured-posts .meta').each(function(){
+        if($(this).height() > highestMeta){
+            highestMeta = $(this).height();
+        }   
+    });
+    $('.featured-posts .meta').height(highestMeta);
+    $(window).resize(function() {
+        if ($(window).width() > 979){
+            var highestMeta = 0;
+            $('.featured-posts .meta').each(function(){
+                if($(this).height() > highestMeta){
+                    highestMeta = $(this).height();
+                }   
+            });
+            $('.featured-posts .meta').height(highestMeta);
+        } if ($(window).width() > 768){
+            var highestMeta = 0;
+            $('.featured-posts .meta').each(function(){
+                if($(this).height() > highestMeta){
+                    highestMeta = $(this).height();
+                }   
+            });
+            $('.featured-posts .meta').height(highestMeta);
+        } else {
+            $('.featured-posts .meta').css('height', 'auto');
+        }
+    });
+
+
+    /// Fix audioboo embed height. Needs to be done with jQuery because of shared class names
+    $('.embedly-embed').css({'position':'relative'}).parent().css({'padding-top':'0' , 'padding-bottom':'0' ,  'height':'auto'});
+
 })
