9fb92419574aa069b59e56e5ebfcfd1636fbf23b
ryan.holder <ryan@dxw.com>
2014-07-04 13:40:41 +0100

    Make space and add extra link into footer. Delivers Ticket #3262

--- templates/footer.php
+++ templates/footer.php
@@ -1,6 +1,7 @@
 <nav>
   <ul id="menu-footer" class="menu">
     <li class="menu-all-government-blogs"><a href="https://www.blog.gov.uk">All GOV.UK blogs</a></li>
+    <li class="menu-all-government-blog-posts"><a href="https://www.blog.gov.uk/all-posts/">All GOV.UK blog posts</a></li>
     <li class="menu-gov-uk"><a href="https://www.gov.uk">GOV.UK</a></li>
     <li class="menu-all-departments"><a href="https://www.gov.uk/government/organisations">All departments</a></li>
     <li class="menu-all-topics"><a href="https://www.gov.uk/government/topics">All topics</a></li>
