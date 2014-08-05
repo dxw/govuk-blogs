7b0939299d2c65da7ffe905b4939cd478011189f
Tom Adams <tom@dxw.com>
2014-08-05 11:25:48 -0400

    Check function_exists

--- lib/options.php
+++ lib/options.php
@@ -1,6 +1,8 @@
 <?php
 
-acf_add_options_sub_page([
-  'title' => 'Theme Options',
-  'parent' => 'themes.php',
-]);
+if (function_exists('acf_add_options_sub_page')) {
+  acf_add_options_sub_page([
+    'title' => 'Theme Options',
+    'parent' => 'themes.php',
+  ]);
+}
