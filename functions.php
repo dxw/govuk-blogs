95fd23cbf9bf33be690adb3dd50d6fb2f3f45a30
Tom Adams <tom@dxw.com>
2016-07-05 15:20:11 -0400

    Use Atom
    Ticket: https://dxw.zendesk.com/agent/tickets/6161

--- functions.php
+++ functions.php
@@ -40,3 +40,5 @@ require(__DIR__.'/lib/about-widget.php');
 require(__DIR__.'/lib/google-maps.php');
 require(__DIR__.'/lib/tableau-shortcode.php');
 require(__DIR__.'/lib/disable-wptexturize.php');
+
+(new \Dxw\Iguana\Extras\UseAtom())->register();
