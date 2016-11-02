d2992680c57d4e837f85a8640cb432eb96fc4c96
Alex Sexton <alex@dxw.com>
2016-11-02 15:31:08 +0000

    (Feature): Add complementary role to aside

--- templates/sidebar.php
+++ templates/sidebar.php
@@ -1,4 +1,4 @@
-<aside class="sidebar">
+<aside class="sidebar" role="complementary">
   <?php dynamic_sidebar('sidebar') ?>
   <?php if (!is_home()) { dynamic_sidebar('page-sidebar'); }
   else { dynamic_sidebar('home-sidebar'); } ?>
