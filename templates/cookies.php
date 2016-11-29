61b64e3ef4698da0241aded4324f433ba49ff2cb
Magda Faizov <magda@dxw.com>
2016-11-29 15:46:47 +0000

    Card 190 - reviewed spacing, made gutters in line with gov.uk styling

--- templates/cookies.php
+++ templates/cookies.php
@@ -1,11 +1,5 @@
 <div class="cookie-message full-width">
-  <div class="container">
-    <div class="row">
-      <div class="span12">
-        <p>
-          GOV.UK blogs use cookies to make the site simpler. <a href="https://blog.gov.uk/cookies/">Find out more about cookies</a>
-        </p>
-      </div>
-    </div>
-  </div>
+  <p>
+    GOV.UK blogs use cookies to make the site simpler. <a href="https://blog.gov.uk/cookies/">Find out more about cookies</a>
+  </p>
 </div>
--- templates/featured.php
+++ templates/featured.php
@@ -1,22 +1,24 @@
 <?php if (gds_get_featured()) : ?>
   <?php while (have_posts()): the_post() ?>
     <div class="row">
-      <article <?php post_class('featured span12') ?>>
-        <div class="featured-wrapper">
-          <h2 class="entry-title visible-tablet"><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h2>
-          <div class="image">
-            <?php gds_post_thumbnail(true) ?>
-          </div>
-          <div class="text">
-            <h2 class="entry-title visible-desktop hidden-tablet"><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h2>
-            <div class="entry-summary">
-              <?php the_excerpt() ?>
+      <div class="span12">
+        <article <?php post_class('featured') ?>>
+          <div class="featured-wrapper">
+            <h2 class="entry-title visible-tablet"><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h2>
+            <div class="image">
+              <?php gds_post_thumbnail(true) ?>
             </div>
-            <a class="read-more" href="<?php the_permalink() ?>">Read more</a>
+            <div class="text">
+              <h2 class="entry-title visible-desktop hidden-tablet"><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h2>
+              <div class="entry-summary">
+                <?php the_excerpt() ?>
+              </div>
+              <a class="read-more" href="<?php the_permalink() ?>">Read more</a>
+            </div>
+            <div class="clear"></div>
           </div>
-          <div class="clear"></div>
-        </div>
-      </article>
+        </article>
+      </div>
     </div>
   <?php endwhile ?>
 <?php endif ?>
