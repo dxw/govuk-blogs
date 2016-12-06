1cd73342a53bdff3a03ec7c5171c5e079aab8ba8
Magda Faizov <magda@dxw.com>
2016-12-06 10:37:04 +0000

    Card 223 - fixed page layout

--- templates/content-page.php
+++ templates/content-page.php
@@ -1,6 +1,13 @@
 <?php while (have_posts()) : the_post() ?>
-<article class="page-content">
-  <?php the_content() ?>
-  <?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')) ?>
-</article>
+  <article class="page-content clearfix">
+    <div class="visible-print">
+      <p><?php echo esc_html(get_permalink()) ?></p>
+    </div>
+    <header>
+      <h1 class="entry-title"><?php the_title() ?></h1>
+    </header>
+    <div class="entry-content">
+      <?php the_content() ?>
+    </div>
+  </article>
 <?php endwhile ?>
