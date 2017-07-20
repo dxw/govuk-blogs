7b4cf7d4fd88a34fca982f24990b6c1aac49cb3f
Magda Faizov <magda@dxw.com>
2017-07-20 15:39:02 +0100

    (chore) card 215, comments and forms styles, updated author containers

--- templates/author.php
+++ templates/author.php
@@ -1,9 +1,9 @@
 <div class="author-container">
     <div class="grid-row author-details">
-        <div class="span2 avatar">
+        <div class="column-one-quarter avatar">
             <?php gds_avatar() ?>
         </div>
-        <div class="span6">
+        <div class="column-three-quarters">
             <h2 class="author-title"><?php the_author_meta('display_name'); ?></h2>
             <?php $content = get_the_author_meta('description');
                 echo apply_filters('the_content', $content); ?>
