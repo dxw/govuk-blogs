ded02367342e0483d53a258ec432c9bbd4618e23
Rob Skilling <rob@dxw.com>
2017-07-24 11:09:53 +0100

    Add hidden text to post meta
    Part of:
    https://trello.com/c/gIyAH45Q/210-1-add-hidden-text-and-nav-elements-to-posts

--- templates/entry-meta.php
+++ templates/entry-meta.php
@@ -1,5 +1,6 @@
 <p class="byline author vcard">
-  <?php gds_byline(); ?>, <time class="updated" datetime="<?php echo get_the_time('c') ?>" pubdate><?php echo get_the_date('j F Y') ?></time>
+  <?php gds_byline(); ?>, <span class="visuallyhidden">Posted on: </span><time class="updated" datetime="<?php echo get_the_time('c') ?>" pubdate><?php echo get_the_date('j F Y') ?></time>
   —
+  <span class="visuallyhidden">Categories: </span>
   <?php echo get_the_category_list(', ') ?>
 </p>
