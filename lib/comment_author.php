aaf2f901b12431dc3400c66217bc3b13bea7863e
Magda Faizov <magda@dxw.com>
2017-07-24 14:12:36 +0100

    (feature) card 212, modified the comments author function

--- lib/comment_author.php
+++ lib/comment_author.php
@@ -4,14 +4,9 @@ function comment_author_page() {
     global $comment;
 
     if ($comment->user_id == '0') {
-        if (!empty ($comment->comment_author_url)) {
-            $url = $comment->comment_author_url;
-        } else {
-            $url = '#';
-        }
+        echo get_comment_author();
     } else {
         $url = get_author_posts_url($comment->user_id);
+        echo "<a href=\"" . $url . "\">" .get_comment_author() . "</a>";
     }
-
-    echo "<a href=\"" . $url . "\">" .get_comment_author() . "</a>";
 }
