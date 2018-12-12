f99489c7587ad5166740f1381ec04a64088e6ab6
Mallory Adams <mallory@dxw.com>
2018-12-12 13:51:32 -0500

    (feature) Add notice explaining that a post is private
    Some users have not realised that private posts were private causing
    confusion.
    
    This change hopefully removes any confusion.
    
    Resolves: https://dxw.zendesk.com/agent/tickets/9209
    Resolves: https://trello.com/c/5Yig50Hh

--- templates/base.php
+++ templates/base.php
@@ -2,6 +2,11 @@
 <div class="container">
   <?php do_action('get_header') ?>
   <?php get_template_part('templates/header') ?>
+  <?php if (get_post_status() === 'private') : ?>
+      <div class="private-notice">
+          <p><strong>This content is private.</strong> It cannot be viewed by anybody except logged in users of this site.</p>
+      </div>
+  <?php endif ?>
   <?php if (is_home()) : ?>
     <?php get_template_part('templates/featured') ?>
   <?php endif ?>
