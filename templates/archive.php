8a387f1d00832aeb67986664d9908e67823ec194
Calum Ryan <dev@calumryan.com>
2020-04-09 15:03:50 +0100

    Change class names in markup for archive

--- templates/archive.php
+++ templates/archive.php
@@ -1,15 +1,15 @@
-<h2 class="archive-title"><?php single_cat_title(); ?></h2>
+<hr class="govuk-section-break govuk-section-break--l govuk-section-break--visible">
+<h2 class="govuk-heading-l"><?php single_cat_title(); ?></h2>
 <?php echo category_description(); ?>
 <?php while (have_posts()) : the_post() ?>
 <article <?php post_class() ?>>
   <header>
-    <h2 class="entry-title"><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h2>
+    <h2 class="govuk-heading-m"><a class="govuk-link" href="<?php the_permalink() ?>"><?php the_title() ?></a></h2>
     <?php get_template_part('templates/entry-meta') ?>
     <?php gds_post_thumbnail() ?>
   </header>
-  <div class="entry-summary">
-    <?php the_excerpt() ?>
-  </div>
+  <?php the_excerpt() ?>
+  
   <?php get_template_part('templates/entry-footer') ?>
 </article>
 <?php endwhile ?>
