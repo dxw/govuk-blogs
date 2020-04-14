b9977d94586238a98de39fc3b42b9406717eccb9
Calum Ryan <dev@calumryan.com>
2020-04-14 16:24:40 +0100

    a11y invalid aside inside main change to div

--- templates/sidebar.php
+++ templates/sidebar.php
@@ -1,4 +1,4 @@
-<aside class="sidebar" role="complementary">
+<div class="sidebar">
   <h2 class="visuallyhidden">Related content and links</h2>
   <?php dynamic_sidebar('sidebar') ?>
   <?php if (!is_home()) {
@@ -6,4 +6,4 @@
 } else {
     dynamic_sidebar('home-sidebar');
 } ?>
-</aside>
+</div>
