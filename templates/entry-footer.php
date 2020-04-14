3a91074a5937ca6f82401d477d6dc8991f6fa729
Rob Skilling <rob@dxw.com>
2020-04-14 11:39:52 +0100

    Use standard govuk frontend classes for footer/meta text
    Rather than custom classes to achieve a similar effect.
    
    This requires a minor tweak to the css to keep margins consistent.
    
    Resolves: https://trello.com/c/9BqhMWPv/499-template-to-migrate-authorphp

--- templates/entry-footer.php
+++ templates/entry-footer.php
@@ -1,4 +1,4 @@
-<footer class="entry-footer">
+<footer class="govuk-body-s">
   <a class="read-more" href="<?php the_permalink() ?>">Read more <span class="govuk-visually-hidden">about this topic</span></a>
   <?php if (get_comments_number() != 0) { ?>
       - <a href="<?php comments_link() ?>" class="view-comments"><?php printf(_n('1 comment', '%1$s comments', get_comments_number(), 'roots'), number_format_i18n(get_comments_number())) ?></a>
--- templates/entry-meta.php
+++ templates/entry-meta.php
@@ -1,4 +1,4 @@
-<div class="byline author vcard">
+<div class="govuk-body-s">
   <?php gds_byline(); ?>, <span class="govuk-visually-hidden">Posted on: </span><time class="updated" datetime="<?php echo get_the_time('c') ?>" pubdate><?php echo get_the_date('j F Y') ?></time>
   -
   <span class="govuk-visually-hidden">Categories: </span>
