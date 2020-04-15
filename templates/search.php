bcfb18eb647d14d20b097685836a99a9932c141c
Calum Ryan <dev@calumryan.com>
2020-04-15 10:01:43 +0100

    Missing quote mark

--- templates/search.php
+++ templates/search.php
@@ -6,7 +6,7 @@
   <?php while (have_posts()) : the_post() ?>
     <article <?php post_class() ?>>
       <header>
-        <h3 class=govuk-heading-m"><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h3>
+        <h3 class="govuk-heading-m"><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h3>
         <?php get_template_part('templates/entry-meta') ?>
         <?php if (has_post_thumbnail()) {
     the_post_thumbnail('large');
