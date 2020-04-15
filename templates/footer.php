ecdf895b6aaa34427ef356c39145943cdfcd3a9a
Calum Ryan <dev@calumryan.com>
2020-04-15 11:54:06 +0100

    Add govuk class names

--- templates/footer.php
+++ templates/footer.php
@@ -1,11 +1,11 @@
 <nav>
-  <h2 class="visuallyhidden">Useful links</h2>
-  <ul id="menu-footer" class="menu">
-    <li class="menu-all-government-blogs"><a href="https://www.blog.gov.uk">All GOV.UK blogs</a></li>
-    <li class="menu-all-government-blog-posts"><a href="https://www.blog.gov.uk/all-posts/">All GOV.UK blog posts</a></li>
-    <li class="menu-gov-uk"><a href="https://www.gov.uk">GOV.UK</a></li>
-    <li class="menu-all-departments"><a href="https://www.gov.uk/government/organisations">All departments</a></li>
-    <li class="menu-a11y"><a href="https://www.blog.gov.uk/accessibility-statement/">Accessibility statement</a></li>
-    <li class="menu-cookies"><a href="https://www.blog.gov.uk/cookies">Cookies</a></li>
+  <h2 class="govuk-visually-hidden">Useful links</h2>
+  <ul id="menu-footer" class="govuk-footer__inline-list">
+    <li class="govuk-footer__inline-list-item menu-all-government-blogs"><a class="govuk-footer__link" href="https://www.blog.gov.uk">All GOV.UK blogs</a></li>
+    <li class="govuk-footer__inline-list-item menu-all-government-blog-posts"><a class="govuk-footer__link" href="https://www.blog.gov.uk/all-posts/">All GOV.UK blog posts</a></li>
+    <li class="govuk-footer__inline-list-item menu-gov-uk"><a class="govuk-footer__link" href="https://www.gov.uk">GOV.UK</a></li>
+    <li class="govuk-footer__inline-list-item menu-all-departments"><a class="govuk-footer__link" href="https://www.gov.uk/government/organisations">All departments</a></li>
+    <li class="govuk-footer__inline-list-item menu-a11y"><a class="govuk-footer__link" href="https://www.blog.gov.uk/accessibility-statement/">Accessibility statement</a></li>
+    <li class="govuk-footer__inline-list-item menu-cookies"><a class="govuk-footer__link" href="https://www.blog.gov.uk/cookies">Cookies</a></li>
   </ul>
 </nav>
