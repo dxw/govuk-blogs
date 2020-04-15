6e8075dc6c6c4e93b5c37234962039e40bc7a5d5
Calum Ryan <dev@calumryan.com>
2020-04-15 13:06:12 +0100

    Migrate licence and footer template to govuk frontend

--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,4 @@
-<nav>
-  <h2 class="govuk-visually-hidden">Useful links</h2>
+
   <ul id="menu-footer" class="govuk-footer__inline-list">
     <li class="govuk-footer__inline-list-item menu-all-government-blogs"><a class="govuk-footer__link" href="https://www.blog.gov.uk">All GOV.UK blogs</a></li>
     <li class="govuk-footer__inline-list-item menu-all-government-blog-posts"><a class="govuk-footer__link" href="https://www.blog.gov.uk/all-posts/">All GOV.UK blog posts</a></li>
@@ -8,4 +7,3 @@
     <li class="govuk-footer__inline-list-item menu-a11y"><a class="govuk-footer__link" href="https://www.blog.gov.uk/accessibility-statement/">Accessibility statement</a></li>
     <li class="govuk-footer__inline-list-item menu-cookies"><a class="govuk-footer__link" href="https://www.blog.gov.uk/cookies">Cookies</a></li>
   </ul>
-</nav>
--- templates/licence.php
+++ templates/licence.php
@@ -1,1 +1,1 @@
-<p>All content is available under the <a href="https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/" rel="license">Open Government Licence v3.0</a>, except where otherwise stated</p>
+<span class="govuk-footer__licence-description">All content is available under the <a class="govuk-footer__link" href="https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/" rel="license">Open Government Licence v3.0</a>, except where otherwise stated</span>
