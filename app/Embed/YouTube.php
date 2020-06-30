a49f3dd8579bbd92cc02805d0dbe63304298a533
Mallory Adams <mallory@dxw.com>
2020-06-30 13:58:07 -0400

    Use _s as delimiters in preg_* functions
    For readability and to avoid unnecessary escaping.

--- app/Embed/YouTube.php
+++ app/Embed/YouTube.php
@@ -14,7 +14,7 @@ class YouTube implements \Dxw\Iguana\Registerable
     // Force to HTTPS
     public function forceHttps(string $cache) : string
     {
-        $cache = preg_replace('$http://www.youtube.com$', 'https://www.youtube.com', $cache);
+        $cache = preg_replace('_http://www.youtube.com_', 'https://www.youtube.com', $cache);
         return $cache;
     }
 
@@ -22,14 +22,14 @@ class YouTube implements \Dxw\Iguana\Registerable
     // https://wordpress.org/plugins/hide-youtube-related-videos/
     public function hideRelated(string $data) : string
     {
-        $data = preg_replace('/(youtube\.com.*)(\?feature=oembed)(.*)/', '$1?wmode=transparent&amp;rel=0$3', $data);
+        $data = preg_replace('_(youtube\.com.*)(\?feature=oembed)(.*)_', '$1?wmode=transparent&amp;rel=0$3', $data);
         return $data;
     }
 
     // Youtube: Add title attribute to iframe
     public function addTitleAttribute(string $return, object $data, string $url) : string
     {
-        if (preg_match('/https?:\/\/((m|www)\.)?youtube\.com\/watch.*/', $url)===1 || preg_match('/https?:\/\/youtu\.be\/.*/', $url)===1) {
+        if (preg_match('_https?://((m|www)\.)?youtube\.com/watch.*_', $url)===1 || preg_match('_https?://youtu\.be/.*_', $url)===1) {
             if (isset($data->title)) {
                 $return = str_replace('></iframe>', ' title="Video: ' . esc_attr($data->title) . '"></iframe>', $return);
             }
