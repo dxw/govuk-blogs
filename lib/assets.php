fb1c977bb7f13fa3afbacf7250a3c048155eedf8
Rob Skilling <rob@dxw.com>
2024-01-26 16:08:46 +0000

    Enqueue script that includes govuk frontend as type module
    This stops errors being thrown about unexpected `export`.
    
    We should probably find a better way of doing this in the real work.
    
    See https://github.com/alphagov/govuk-frontend/blob/main/CHANGELOG.md#stop-internet-explorer-11-and-other-older-browsers-running-unsupported-javascript

--- lib/assets.php
+++ lib/assets.php
@@ -9,6 +9,18 @@ add_action('wp_enqueue_scripts', function () {
     wp_enqueue_style('main', get_template_directory_uri().'/build/main.min.css');
 });
 
+function add_type_attribute($tag, $handle, $src)
+{
+    // if not your script, do nothing and return original $tag
+    if ('main' !== $handle) {
+        return $tag;
+    }
+    // change the script tag by adding type="module" and return it.
+    $tag = '<script type="module" src="' . esc_url($src) . '"></script>';
+    return $tag;
+}
+add_filter('script_loader_tag', 'add_type_attribute', 10, 3);
+
 add_action('admin_enqueue_scripts', function () {
     wp_enqueue_style('admin', get_template_directory_uri().'/build/admin.min.css');
 });
