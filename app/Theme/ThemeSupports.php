4d27129d84f9cdd860252e57509e876b5d86a413
Matthew Passmore <matthew@matthewpassmore.co.uk>
2024-03-15 10:43:27 +0000

    Ensure theme stylesheet is loaded in Block Editor

--- app/Theme/ThemeSupports.php
+++ app/Theme/ThemeSupports.php
@@ -6,6 +6,12 @@ class ThemeSupports implements \Dxw\Iguana\Registerable
 {
 	public function register()
 	{
+		add_action('after_setup_theme', [$this, 'addThemeSupport']);
+	}
+
+	public function addThemeSupport()
+	{
+		add_theme_support('editor-styles');
 		add_theme_support('title-tag');
 	}
 }
