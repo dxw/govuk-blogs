de5f15303ee4f16fb562f5cfe3fdefa6f580fba9
Matthew Passmore <matthew@matthewpassmore.co.uk>
2024-03-19 16:54:28 +0000

    Align PHP syntax with code sniffer rules

--- app/Blocks/InsetText/build/index.asset.php
+++ app/Blocks/InsetText/build/index.asset.php
@@ -1,1 +1,3 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'bf63bb6596f8f846e0cf');
+<?php
+
+return ['dependencies' => ['react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'], 'version' => 'bf63bb6596f8f846e0cf'];
