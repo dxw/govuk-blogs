26287ddcd2df8ebde94571fc0fe57bad46ac36bd
Matthew Passmore <matthew@matthewpassmore.co.uk>
2024-03-19 10:44:42 +0000

    Scaffold block using @wordpress/create-block
    Note that when running the setup script, use lowercase and hyphens, and
    then change the block folder name to CamelCase once setup is complete.
    This enables the Kahlan test to work correctly due to our namespacing
    conventions.
    
    https://developer.wordpress.org/block-editor/reference-guides/packages/packages-create-block/

---
+++ app/Blocks/InsetText/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '22bcbce36b1cfa8a2a5c');
---
+++ app/Blocks/InsetText/build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'a44ca0ea12532069a052');
