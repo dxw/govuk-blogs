67d9778aa4cb005d2b9958312a3f2eb48ac23527
Matthew Passmore <matthew@matthewpassmore.co.uk>
2024-03-19 15:29:00 +0000

    Add example text for the block preview

--- app/Blocks/InsetText/build/index.asset.php
+++ app/Blocks/InsetText/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '37275cf721bb513b9403');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '73e3756ec98d63e2a3c0');
