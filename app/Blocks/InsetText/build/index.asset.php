0399f7e0b2dc4cbccd6c0c2a90a92ee4190e5b93
Matthew Passmore <matthew@matthewpassmore.co.uk>
2024-03-20 10:17:08 +0000

    Remove redundant reference to starter icon

--- app/Blocks/InsetText/build/index.asset.php
+++ app/Blocks/InsetText/build/index.asset.php
@@ -1,3 +1,3 @@
 <?php
 
-return ['dependencies' => ['react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'], 'version' => 'bf63bb6596f8f846e0cf'];
+return ['dependencies' => ['react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'], 'version' => '66913477a78322433a6e'];
