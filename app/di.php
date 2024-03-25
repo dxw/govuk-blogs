2c0ab12efffb97f4ad79b6158aacbec7bfe7e4b5
Sim Brody <sim@dxw.com>
2024-03-25 10:43:45 +0000

    Create function to control which blocks are available in the editor.

--- app/di.php
+++ app/di.php
@@ -8,13 +8,13 @@ $registrar->addInstance(new \Dxw\Iguana\Theme\LayoutRegister(
 
 // Blocks
 $registrar->addInstance(new \GovUKBlogs\Blocks\BlockCategory());
+$registrar->addInstance(new \GovUKBlogs\Blocks\AllowedBlocks());
 $registrar->addInstance(new \GovUKBlogs\Blocks\Details\Block());
 $registrar->addInstance(new \GovUKBlogs\Blocks\InsetText\Block());
 $registrar->addInstance(new \GovUKBlogs\Blocks\Accordion\Block());
 $registrar->addInstance(new \GovUKBlogs\Blocks\AccordionRow\Block());
 
 // Theme
-
 $registrar->addInstance(new \GovUKBlogs\Theme\ThemeSupports());
 $registrar->addInstance(new \GovUKBlogs\WidgetCategoriesDropdownWithSubmit\Register());
 $registrar->addInstance(new \GovUKBlogs\WidgetArchiveDropdownWithSubmit\Register());
---
+++ spec/blocks/allowed_blocks.spec.php
@@ -0,0 +1,37 @@
+<?php
+
+use Kahlan\Plugin\Double;
+
+describe(GovUKBlogs\Blocks\AllowedBlocks::class, function () {
+	beforeEach(function () {
+		$this->allowedBlocks = new GovUKBlogs\Blocks\AllowedBlocks();
+	});
+
+	it('implements \Dxw\Iguana\Registerable', function () {
+		expect($this->allowedBlocks)->toBeAnInstanceOf(\Dxw\Iguana\Registerable::class);
+	});
+
+	describe('->register()', function () {
+		it('adds the filter', function () {
+			allow('add_filter')->toBeCalled();
+			expect('add_filter')->toBeCalled()->once()->with('allowed_block_types_all', [$this->allowedBlocks, 'limitAllowedBlocks']);
+
+			$this->allowedBlocks->register();
+		});
+	});
+
+	describe('->limitAllowedBlocks)', function () {
+		it('limits the blocks allowed in the block editor', function () {
+			$arg1 = [
+				'core/post',
+				'core/list'
+			];
+			$arg2 = Double::instance([
+				'class' => '\WP_Block_Editor_Context'
+			]);
+			$result = $this->allowedBlocks->limitAllowedBlocks($arg1, $arg2);
+			expect($result)->toBeA('object');
+			$this->allowedBlocks->limitAllowedBlocks($arg1, $arg2);
+		});
+	});
+});
