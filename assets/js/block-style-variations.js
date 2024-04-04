553c456da5fc79f4a89536a37edbfeae4e54b1b0
Sim Brody <sim@dxw.com>
2024-04-04 13:54:41 +0100

    Add styling for separator block
    Styling in line with GOV.UK style guide. Assumption is made that only visible styles are needed and three visible style options are provided.

--- assets/js/block-style-variations.js
+++ assets/js/block-style-variations.js
@@ -19,13 +19,41 @@ wp.blocks.registerBlockStyle( 'core/button', {
 	label: 'Inverse'
 } );
 
+wp.blocks.registerBlockStyle( 'core/separator', {
+	name: 'medium',
+	label: 'Medium',
+	isDefault: true
+} );
+
+wp.blocks.registerBlockStyle( 'core/separator', {
+	name: 'large',
+	label: 'Large'
+} );
+
+wp.blocks.registerBlockStyle( 'core/separator', {
+	name: 'x-large',
+	label: 'Extra large'
+} );
+
 wp.domReady(function(){
 	wp.blocks.unregisterBlockStyle( 'core/button', 'fill' );
-})
+});
 
 wp.domReady(function(){
 	wp.blocks.unregisterBlockStyle( 'core/button', 'outline' );
-})
+});
+
+wp.domReady(function(){
+	wp.blocks.unregisterBlockStyle( 'core/separator', 'default' );
+});
+
+wp.domReady(function(){
+	wp.blocks.unregisterBlockStyle( 'core/separator', 'dots' );
+});
+
+wp.domReady(function(){
+	wp.blocks.unregisterBlockStyle( 'core/separator', 'wide' );
+});
 
 wp.domReady(function(){
 	wp.blocks.unregisterBlockStyle( 'core/quote', ['plain', 'default'] );
