d8ed1e812a994338a100333f101d395e142f42d8
Sim Brody <sim@dxw.com>
2024-04-10 11:52:30 +0100

    Adjust styling to more closely match search bar on gov.uk sites.
    Search bar style and accessibility improvements
    
    New GOV.UK search icon introduced. Search label made visible and placeholder removed. JavaScript which was used to remove placeholder on click is now removed as it is redundant. Additional search input markup introduced as per GOV.UK to aid accessibility.

--- templates/searchform.php
+++ templates/searchform.php
@@ -1,7 +1,7 @@
 <form role="search" method="get" id="searchform" class="form-search" action="<?php echo home_url('/') ?>">
-    <label class="govuk-visually-hidden" for="s">Search for:</label>
+    <label class="govuk-label govuk-label--m" for="s">Search blog</label>
     <div class="search-input-wrapper">
-        <input type="search" value="<?php echo get_search_query() ?>" name="s" id="s" class="search-query" placeholder="Search blog">
+        <input enterkeyhint="search" title="Search" type="search" value="<?php echo get_search_query() ?>" name="s" id="s" class="search-query">
         <input type="submit" id="searchsubmit" value="<?php _e('Search', 'roots') ?>" class="btn">
     </div>
 </form>
