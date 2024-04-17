bf9dbcb7545c83180e6c0dff2ad2d05ff6e5b47a
Sim Brody <sim@dxw.com>
2024-04-17 09:31:56 +0100

    Remove function preventing display of file embeds. Update styling.

--- lib/custom.php
+++ lib/custom.php
@@ -5,13 +5,6 @@ add_action('widgets_init', function () {
 	unregister_widget('WP_Widget_Search');
 }, 1);
 
-
-# Force inserted media to use https urls
-add_filter('wp_get_attachment_url', function ($url, $post_id) {
-	return preg_replace('/^http:\/\//', 'https://', $url);
-}, 10, 2);
-
-
 # Contributors can upload images
 if (current_user_can('contributor') && !current_user_can('upload_files')) {
 	add_action('admin_init', 'allow_contributor_uploads');
