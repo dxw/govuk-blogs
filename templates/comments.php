3c8237b5d34792e495961940a67b8931abc2e56e
Sim Brody <sim@dxw.com>
2024-11-11 17:11:28 +0000

    Move Roots comment walker to a new class.

--- templates/comments.php
+++ templates/comments.php
@@ -8,7 +8,7 @@ if (have_comments()) : ?>
     <h3 class="govuk-heading-m"><?php printf(_n('1 comment', '%1$s comments', get_comments_number(), 'roots'), number_format_i18n(get_comments_number())) ?></h3>
 
     <ol class="media-list">
-      <?php wp_list_comments(['walker' => new Roots_Walker_Comment()]) ?>
+      <?php wp_list_comments(['walker' => new GDS_Walker_Comment()]) ?>
     </ol>
 
     <?php if (get_comment_pages_count() > 1 && get_option('page_comments')) : ?>
