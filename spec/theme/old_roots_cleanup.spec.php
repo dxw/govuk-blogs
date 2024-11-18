ff255f145b94d5c4b9d225c0038134fef71c50ae
Sim Brody <sim@dxw.com>
2024-11-18 15:01:55 +0000

    Reintroduce old Roots function which wrapped images with captions with figure markup. Removing it has caused some old images to break their container.

--- spec/theme/old_roots_cleanup.spec.php
+++ spec/theme/old_roots_cleanup.spec.php
@@ -12,10 +12,11 @@ describe(GovUKBlogs\Theme\OldRootsCleanup::class, function () {
 	describe('->register()', function () {
 		it('adds the filters', function () {
 			allow('add_filter')->toBeCalled();
-			expect('add_filter')->toBeCalled()->times(3);
+			expect('add_filter')->toBeCalled()->times(4);
 			expect('add_filter')->toBeCalled()->with('excerpt_length', [$this->oldRootsCleanup, 'excerptLength']);
 			expect('add_filter')->toBeCalled()->with('excerpt_more', [$this->oldRootsCleanup, 'excerptMore']);
 			expect('add_filter')->toBeCalled()->with('get_bloginfo_rss', [$this->oldRootsCleanup, 'removeDefaultDescription']);
+			expect('add_filter')->toBeCalled()->with('img_caption_shortcode', [$this->oldRootsCleanup, 'gdsCaption']);
 
 			$this->oldRootsCleanup->register();
 		});
@@ -37,6 +38,52 @@ describe(GovUKBlogs\Theme\OldRootsCleanup::class, function () {
 			$more = 'Read more';
 			$value = $this->oldRootsCleanup->excerptMore($more);
 			expect($value)->toEqual(' &hellip; <a href="' . 'http://localhost.com' . '">' . 'Continued' . '</a>');
+		});
+	});
+
+	describe('->gdsCaption()', function () {
+		beforeEach(function () {
+			$this->output = 'a string';
+			$this->attr = [
+				'id' => '0',
+				'align' => 'none',
+				'width'   => 0,
+				'caption' => ''
+			];
+			$this->content = 'the content';
+		});
+		context('the page is a feed', function () {
+			it('returns the original string', function () {
+				allow('is_feed')->toBeCalled()->andReturn(true);
+				$result = $this->oldRootsCleanup->gdsCaption($this->output, $this->attr, $this->content);
+				expect($result)->toEqual('a string');
+			});
+		});
+		context('there is no caption and image width', function () {
+			it('returns the content', function () {
+				allow('is_feed')->toBeCalled()->andReturn(false);
+				allow('shortcode_atts')->toBeCalled()->andReturn($this->attr);
+				$result = $this->oldRootsCleanup->gdsCaption($this->output, $this->attr, $this->content);
+				expect($result)->toEqual('the content');
+			});
+		});
+		context('there is an image with a caption', function () {
+			it('returns a custom string', function () {
+				$this->attr = [
+					'id' => '0',
+					'align' => 'none',
+					'width'   => '10',
+					'caption' => 'a caption'
+				];
+				allow('is_feed')->toBeCalled()->andReturn(false);
+				allow('shortcode_atts')->toBeCalled()->andReturn($this->attr);
+				allow('esc_attr')->toBeCalled()->andReturn('0');
+				allow('esc_attr')->toBeCalled()->andReturn('none');
+				allow('esc_attr')->toBeCalled()->andReturn('10');
+				allow('do_shortcode')->toBeCalled()->with('the content')->andReturn('the content');
+				$result = $this->oldRootsCleanup->gdsCaption($this->output, $this->attr, $this->content);
+				expect($result)->toEqual('<figure class="thumbnail wp-caption 10" style="width: 10px">the content<figcaption class="caption wp-caption-text">a caption</figcaption></figure>');
+			});
 		});
 	});
 });
