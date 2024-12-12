44d04edf146d9247f727b80033fb0bf05b105127
Sarah Mount <sarah.mount@dxw.com>
2024-12-12 17:23:12 +0000

    Apply linter fixes

--- lib/byline.php
+++ lib/byline.php
@@ -5,8 +5,8 @@ function gds_byline()
 {
 	?>
 	<span class="govuk-visually-hidden">Posted by: </span><?php
-	$tmp_author_id = get_post_field( 'post_author', $post->ID );
-	$tmp_user =  get_user_by('id', $author_id );
+	$tmp_author_id = get_post_field('post_author', $post->ID);
+	$tmp_user =  get_user_by('id', $author_id);
 	if (empty($tmp_user->user_login)) {
 		echo "Deleted user $tmp_author_id";
 		error_log("{$post->ID} has deleted user $tmp_author_id", 0);
