a2211c3b13f006f305011e1170135bec2a94e2f7
Lee Maguire <lee@dxw.com>
2025-01-16 11:10:16 +0000

    Set default licence to avoid warnings
    (Note use of en_GB "licence" except in rel which uses en_US "license")
    
    * set the "other" licence if not specified
    * change the program flow to have one return point
    * use 'rel="license"'
    * separate the licence and attribution with a comma
    
    ```
    PHP Warning:  Undefined array key "" in /var/www/html/wp-content/themes/gds-blogs/lib/image-licensing.php on line 52;
    PHP Warning:  Trying to access array offset on null in /var/www/html/wp-content/themes/gds-blogs/lib/image-licensing.php on line 57
    ```
    
    * https://html.spec.whatwg.org/multipage/links.html#link-type-license

--- lib/image-licensing.php
+++ lib/image-licensing.php
@@ -47,19 +47,33 @@ $gds_image_licences = [
 
 add_filter('image_send_to_editor', function ($html, $id, $caption, $title, $align, $url, $size, $alt) {
 	global $gds_image_licences;
+	$caption = '';
 
 	$_licence = get_post_meta($id, 'licence', true);
-	$licence = $gds_image_licences[$_licence];
+	if (!empty($_licence)) {
+		$licence = $gds_image_licences[$_licence];
+	} else {
+		$licence = $gds_image_licences['other'];
+	}
+
 	$copyright_holder = get_post_meta($id, 'copyright_holder', true);
 	$link_to_source = get_post_meta($id, 'link_to_source', true);
 
-	$caption = 'Licence: ';
-	if ($licence['display'] === true) {
-		$caption .= '<a href="'.esc_attr($licence['link']).'">'.esc_html($licence['name']).'</a>';
+	if ($licence['display'] === true || !empty($copyright_holder)) {
+		if ($licence['display'] === true) {
+			$caption .= __('Licence:');
+			$caption .= ' <a rel="license" href="'.esc_attr($licence['link']).'">'.esc_html($licence['name']).'</a>';
+			if (!empty($copyright_holder)) {
+				$caption .= ', ';
+			}
+		}
+		if (!empty($copyright_holder)) {
+			$caption .= '<a href="'.esc_attr($link_to_source).'">'.esc_html($copyright_holder).'</a>';
+		}
+		$output_html = '<figure class="thumbnail">'.$html.'<figcaption class="caption">'.$caption.'</figcaption></figure>';
 	} else {
-		return $html;
+		$output_html = $html;
 	}
-	$caption .= ' <a href="'.esc_attr($link_to_source).'">'.esc_html($copyright_holder).'</a>';
 
-	return '<figure class="thumbnail">'.$html.'<figcaption class="caption">'.$caption.'</figcaption></figure>';
+	return $output_html;
 }, 999, 8);
