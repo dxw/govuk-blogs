8319fa2866a4bab5d3132008c7f6b33ae3624570
Lee Maguire <lee@dxw.com>
2025-03-07 16:42:34 +0000

    Add i18n hooks
    Note: changes text-domain to "govuk-blogs"
    
    https://developer.wordpress.org/themes/functionality/internationalization/

--- templates/cookies.php
+++ templates/cookies.php
@@ -1,5 +1,6 @@
 <div class="cookie-message full-width">
 	<p class="govuk-body">
-		GOV.UK blogs use cookies to make the site simpler. <a href="https://www.blog.gov.uk/cookies/">Find out more about cookies</a>
+		<?php _e("GOV.UK blogs use cookies to make the site simpler.", "govuk-blogs"); ?>
+		<a href="https://www.blog.gov.uk/cookies/"><?php _e("Find out more about cookies", "govuk-blogs"); ?></a>
 	</p>
 </div>
--- templates/entry-footer.php
+++ templates/entry-footer.php
@@ -1,5 +1,5 @@
 <footer class="govuk-body-s">
-  <a class="govuk-link" href="<?php the_permalink() ?>"><strong>Read more</strong> <span class="govuk-visually-hidden">of <?php the_title() ?></span></a>
+  <a class="govuk-link" href="<?php the_permalink() ?>"><?php echo sprintf(__('<strong>Read more</strong><span class="govuk-visually-hidden"> of %s</span>', "govuk-blogs"), get_the_title()); ?></a>
   <?php if (get_comments_number() != 0) { ?>
       - <a href="<?php comments_link() ?>" class="govuk-link"><?php printf(_n('1 comment', '%1$s comments', get_comments_number(), 'roots'), number_format_i18n(get_comments_number())) ?></a>
     <?php } ?>
