0644932bbbf7f22353ffa3d5481e8e79d9a6da40
Lee Maguire <lee@dxw.com>
2025-03-08 18:03:57 +0000

    Update cookies URL

--- templates/cookies.php
+++ templates/cookies.php
@@ -1,6 +1,6 @@
 <div class="cookie-message full-width">
   <p class="govuk-body">
     <?php _e("GOV.UK blogs use cookies to make the site simpler", "govuk-blogs");
-    ?>. <a href="https://blog.gov.uk/cookies/"><?php _e("Find out more about cookies", "govuk-blogs"); ?></a>
+    ?>. <a href="https://www.blog.gov.uk/cookies/"><?php _e("Find out more about cookies", "govuk-blogs"); ?></a>
   </p>
 </div>
