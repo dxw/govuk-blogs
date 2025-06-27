2192eb8553d21802e1d7a4adec7c19f96018f1b2
Lee Maguire <lee@dxw.com>
2025-06-28 00:37:00 +0100

    Fix the Cookie URLs
    - Correct url for Cookie info
    - Whitespace changes, to better match standard GOV.UK

--- templates/cookies.php
+++ templates/cookies.php
@@ -1,5 +1,5 @@
 <div class="cookie-message full-width">
-  <p class="govuk-body">
-    GOV.UK blogs use cookies to make the site simpler. <a href="https://blog.gov.uk/cookies/">Find out more about cookies</a>
-  </p>
+	<p class="govuk-body">
+		GOV.UK blogs use cookies to make the site simpler. <a href="https://www.blog.gov.uk/cookies/">Find out more about cookies</a>
+	</p>
 </div>
