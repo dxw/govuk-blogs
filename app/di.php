680cb6046814cf066d664498ddfb31206d1c305c
patdel0 <zefiru@protonmail.com>
2025-07-03 15:11:00 +0100

    Use ImageLicensing for license choices

--- app/di.php
+++ app/di.php
@@ -21,6 +21,7 @@ $registrar->addInstance(new \GovUKBlogs\WidgetArchiveDropdownWithSubmit\Register
 $registrar->addInstance(new \GovUKBlogs\OpenGraphImage());
 $registrar->addInstance(new \GovUKBlogs\Embed\YouTube());
 $registrar->addInstance(new \GovUKBlogs\Theme\CSSManifest(dirname(__FILE__, 2) . '/build/fingerprint.json'));
+$registrar->addInstance(new \GovUKBlogs\Theme\ImageLicensing());
 $registrar->addInstance(new \GovUKBlogs\Theme\Scripts(
 	$registrar->getInstance(\GovUKBlogs\Theme\CSSManifest::class)
 ));
