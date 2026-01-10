3ef15278dd8ab6f88dbcf537fe08346643081041
Lee Maguire <lee@dxw.com>
2026-01-10 11:15:25 +0000

    Add a network menu to control individual sharing icons
    Allow a network admin to switch sharing icons on an off.
    
    In the event that a sharing destination is no longer available,
    a network admin can disable its sharing icon network-wide.

--- lib/share-icons.php
+++ lib/share-icons.php
@@ -1,5 +1,66 @@
 <?php
 
+function share_icons_admin()
+{
+	add_submenu_page('settings.php', 'Sharing links', 'Sharing links', 'manage_network', 'sharing-links', function () {
+
+		if (isset($_POST['_wpnonce']) && wp_verify_nonce($_POST['_wpnonce'], 'sharing_links')) {
+			if (isset($_POST['share_twitter'])) {
+				update_site_option('sharing_links_twitter', 1);
+			} else {
+				update_site_option('sharing_links_twitter', 0);
+			}
+			if (isset($_POST['share_facebook'])) {
+				update_site_option('sharing_links_facebook', 1);
+			} else {
+				update_site_option('sharing_links_facebook', 0);
+			}
+			if (isset($_POST['share_linkedin'])) {
+				update_site_option('sharing_links_linkedin', 1);
+			} else {
+				update_site_option('sharing_links_linkedin', 0);
+			}
+			if (isset($_POST['share_email'])) {
+				update_site_option('sharing_links_email', 1);
+			} else {
+				update_site_option('sharing_links_email', 0);
+			}
+		}
+		?>
+<div class="wrap">
+	<h2>Sharing links</h2>
+
+		<form method="POST">
+			<?php wp_nonce_field('sharing_links') ?>
+
+			<p>
+				Control which sharing options are displayed at the bottom of the article.
+			</p>
+			<p>
+				This does not affect profile links shown elsewhere.
+			</p>
+
+			<input type="checkbox" id="share_twitter" name="share_twitter" value="1"<?= checked(get_site_option('sharing_links_twitter'), 1) ?>>
+			<label for="share_twitter">Twitter</label><br>
+
+			<input type="checkbox" id="share_facebook" name="share_facebook" value="1"<?= checked(get_site_option('sharing_links_facebook'), 1) ?>>
+			<label for="share_facebook">Facebook</label><br>
+
+			<input type="checkbox" id="share_linkedin" name="share_linkedin" value="1"<?= checked(get_site_option('sharing_links_linkedin'), 1) ?>>
+			<label for="share_linkedin">LinkedIn</label><br>
+
+			<input type="checkbox" id="share_email" name="share_email" value="1"<?= checked(get_site_option('sharing_links_email'), 1) ?>>
+			<label for="share_email">Email</label><br>
+
+			<?php submit_button() ?>
+	</form>
+</div>
+<?php
+	});
+}
+
+add_action('network_admin_menu', 'share_icons_admin');
+
 function share_icons($id)
 {
 	$url = get_permalink($id);
@@ -23,24 +84,48 @@ function share_icons($id)
 
 	?>
 
-  <div class="icons-buttons">
-    <h3><?php echo __("Share this page"); ?></h3>
-    <ul>
-      <li>
-        <a target="_blank" href="<?php echo esc_attr($twitter_url) ?>" class="twitter">Twitter</a>
-      </li>
-      <li>
-        <a target="_blank" href="<?php echo esc_attr($facebook_url) ?>" class="facebook">Facebook</a>
-      </li>
-      <li>
-        <a target="_blank" href="<?php echo esc_attr($linkedin_url) ?>" class="linkedin">LinkedIn</a>
-      </li>
-      <li>
-        <a href="<?php echo esc_attr($mailto_url); ?>" class="email">Email</a>
-      </li>
-    </ul>
-    <div class="govuk-clearfix"></div>
-  </div>
+<div class="icons-buttons">
+	<h3><?php _e("Share this page", "govuk-blogs"); ?></h3>
+	<ul>
 
-  <?php
+		<?php if (get_site_option('sharing_links_twitter')) { ?>
+		<li>
+			<a target="_blank" rel="noopener noreferrer external" class="govuk-link govuk-link--no-underline twitter" href="<?= esc_attr($twitter_url) ?>">
+				<span class="govuk-visually-hidden"><?php _e("Share on", "govuk-blogs"); ?></span>
+				Twitter
+				<span class="govuk-visually-hidden">(<?php _e("opens in new tab", "govuk-blogs"); ?>)</span>
+			</a>
+		</li>
+		<?php } ?>
+		<?php if (get_site_option('sharing_links_facebook')) { ?>
+		<li>
+			<a target="_blank" rel="noopener noreferrer external" class="govuk-link govuk-link--no-underline facebook" href="<?= esc_attr($facebook_url) ?>">
+				<span class="govuk-visually-hidden"><?php _e("Share on", "govuk-blogs"); ?></span>
+				Facebook
+				<span class="govuk-visually-hidden">(<?php _e("opens in new tab", "govuk-blogs"); ?>)</span>
+			</a>
+		</li>
+		<?php } ?>
+		<?php if (get_site_option('sharing_links_linkedin')) { ?>
+		<li>
+			<a target="_blank" rel="noopener noreferrer external" class="govuk-link govuk-link--no-underline linkedin" href="<?= esc_attr($linkedin_url) ?>">
+				<span class="govuk-visually-hidden"><?php _e("Share on", "govuk-blogs"); ?></span>
+				LinkedIn
+				<span class="govuk-visually-hidden">(<?php _e("opens in new tab", "govuk-blogs"); ?>)</span>
+			</a>
+		</li>
+		<?php } ?>
+		<?php if (get_site_option('sharing_links_email')) { ?>
+		<li>
+			<a target="_blank" rel="noopener noreferrer external" class="govuk-link govuk-link--no-underline email" href="<?= esc_attr($mailto_url) ?>">
+				<span class="govuk-visually-hidden"><?php _e("Share on", "govuk-blogs"); ?></span>
+				<?php _e("Email", "govuk-blogs"); ?>
+				<span class="govuk-visually-hidden">(<?php _e("opens in new tab", "govuk-blogs"); ?>)</span>
+			</a>
+		</li>
+		<?php } ?>
+	</ul>
+	<div class="govuk-clearfix"></div>
+</div>
+<?php
 }
