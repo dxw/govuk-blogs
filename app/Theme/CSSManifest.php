de826e3b6a425b8b08ccd1f57c6c6d07a7e7e862
patdel0 <zefiru@protonmail.com>
2026-01-16 16:03:36 +0000

    Resolve `MixedAssignment` error

--- app/Theme/CSSManifest.php
+++ app/Theme/CSSManifest.php
@@ -4,23 +4,29 @@ namespace GovUKBlogs\Theme;
 
 class CSSManifest
 {
-	/** @var array<string, mixed> */
+	/** @var array<array-key, mixed> */
 	private array $json;
 
 	public function __construct(string $pathToCSSManifest)
 	{
-		$this->json = json_decode(file_get_contents($pathToCSSManifest), true);
+		/** @var array<array-key, mixed>|null */
+		$json = json_decode(file_get_contents($pathToCSSManifest), true);
+
+		if (!is_array($json)) {
+			$json = [];
+		}
+
+		$this->json = $json;
 	}
 
 	public function get(string $fileName): string
 	{
-		/** @var array<string,string> */
 		$rewrittenFiles = $this->json['rewrittenFiles'] ?? [];
 
-		if (!array_key_exists($fileName, $rewrittenFiles)) {
+		if (!is_array($rewrittenFiles) || !array_key_exists($fileName, $rewrittenFiles)) {
 			return '';
 		}
 
-		return $rewrittenFiles[$fileName];
+		return (string) $rewrittenFiles[$fileName];
 	}
 }
