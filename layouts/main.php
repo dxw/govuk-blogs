d20b18fd45d9242f40eaddfc7b3ac2a141feb270
patdel0 <zefiru@protonmail.com>
2026-03-17 09:12:03 +0000

    Remove the `--rebranded` flag from the html

--- layouts/main.php
+++ layouts/main.php
@@ -2,7 +2,7 @@
 $govukFrontendAssetPath = get_template_directory_uri() . '/build/node_modules/govuk-frontend/dist/govuk/assets/rebrand/';
 ?>
 <!DOCTYPE html>
-<html lang="en" class="govuk-template--rebranded">
+<html lang="en">
 	<head>
 		<meta charset="utf-8">
 
