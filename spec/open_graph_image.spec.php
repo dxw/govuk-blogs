b00ce6693b0c0666fc7b70384757e65d327b32d8
Brent <brent@dxw.com>
2026-06-05 12:06:19 +0100

    Use either featured image or default fallback for og:image

--- spec/open_graph_image.spec.php
+++ spec/open_graph_image.spec.php
@@ -12,7 +12,8 @@ describe(OpenGraphImage::class, function () {
 	});
 
 	describe('->register()', function () {
-		it('adds action', function () {
+		it('adds action and sets default image', function () {
+			allow('get_template_directory_uri')->toBeCalled()->andReturn('https://govuk-blog.invalid/wp-content/themes/gds-blogs');
 			allow('add_action')->toBeCalled();
 
 			expect('add_action')->toBeCalled()->with('wp_head', [$this->openGraphImage, 'wpHead'])->once();
@@ -25,13 +26,14 @@ describe(OpenGraphImage::class, function () {
 		context('when not on a post/page', function () {
 			beforeEach(function () {
 				allow('is_single')->toBeCalled()->andReturn(false);
+				allow('esc_url')->toBeCalled()->andReturn('https://govuk-blog.invalid/wp-content/themes/gds-blogs/build/govuk-assets/images/govuk-opengraph-image.png');
 			});
 
-			it('does nothing', function () {
+			it('outputs the default og:image tag', function () {
 				ob_start();
 				$this->openGraphImage->wpHead();
 				$output = ob_get_clean();
-				expect($output)->toEqual('');
+				expect($output)->toEqual('<meta property="og:image" content="https://govuk-blog.invalid/wp-content/themes/gds-blogs/build/govuk-assets/images/govuk-opengraph-image.png">'."\n");
 			});
 		});
 
@@ -43,13 +45,14 @@ describe(OpenGraphImage::class, function () {
 			context('when there is no featured image', function () {
 				beforeEach(function () {
 					allow('has_post_thumbnail')->toBeCalled()->andReturn(false);
+					allow('esc_url')->toBeCalled()->andReturn('https://govuk-blog.invalid/wp-content/themes/gds-blogs/build/govuk-assets/images/govuk-opengraph-image.png');
 				});
 
-				it('does nothing', function () {
+				it('outputs the default og:image tag', function () {
 					ob_start();
 					$this->openGraphImage->wpHead();
 					$output = ob_get_clean();
-					expect($output)->toEqual('');
+					expect($output)->toEqual('<meta property="og:image" content="https://govuk-blog.invalid/wp-content/themes/gds-blogs/build/govuk-assets/images/govuk-opengraph-image.png">'."\n");
 				});
 			});
 
@@ -57,6 +60,7 @@ describe(OpenGraphImage::class, function () {
 				beforeEach(function () {
 					allow('has_post_thumbnail')->toBeCalled()->andReturn(true);
 					allow('get_the_post_thumbnail_url')->toBeCalled()->andReturn('https://govuk-blog.invalid/wp-content/uploads/my-image.png');
+					allow('esc_url')->toBeCalled()->andReturn('https://govuk-blog.invalid/wp-content/uploads/my-image.png');
 				});
 
 				it('outputs an og:image tag', function () {
